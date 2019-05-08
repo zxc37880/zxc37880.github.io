5cc25624495209772748c436f19faf7603b8b2ea
Boren <zxc3880@gmail.com>
2019-05-09 00:58:26 +0800

    fixed.

--- SmallFunction/NavHighlight/app.js
+++ SmallFunction/NavHighlight/app.js
@@ -6,6 +6,7 @@ sections.forEach(section => {
     section.addEventListener("mouseenter", function () {
         const id = this.getAttribute("id");
         const navActive = document.querySelector("a[href= '#" + id + "']"); 
+        console.log(navActive);
         navLinks.forEach(link => link.classList.remove("active"));
         navActive.classList.add("active");
     })
