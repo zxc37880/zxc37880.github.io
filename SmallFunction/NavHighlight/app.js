836ab676b043aace4f26300c69bbeb93c1b3a1a5
Boren <zxc3880@gmail.com>
2019-05-09 00:48:12 +0800

    Input Animation & Nav Highlight 小功能.

---
+++ SmallFunction/NavHighlight/app.js
@@ -0,0 +1,16 @@
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+sections.forEach(section => {
+    section.addEventListener("mouseenter", function () {
+        const id = this.getAttribute("id");
+        const navActive = document.querySelector("a[href= '#" + id + "']"); 
+        navLinks.forEach(link => link.classList.remove("active"));
+        navActive.classList.add("active");
+    })
+})
+
+
+
+
